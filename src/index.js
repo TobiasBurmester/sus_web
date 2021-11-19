cb1cc8a1ebecceaf1b9441b76eb718ed824c8fea
TobiasBurmester <94347959+TobiasBurmester@users.noreply.github.com>
2021-11-19 13:39:11 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -7,26 +7,3 @@ if ("serviceWorker" in navigator){
         console.log(error);
     });
 } 
-
-window.addEventListener('resize', r =>{
-    
-
-        var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-        var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-        $("html, body").css({"width":w,"height":h});
-
-
-   //do your layout change here.
-
-});
-
-const deviceType = () => {
-    const ua = navigator.userAgent;
-    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
-        return "tablet";
-    }
-    else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
-        return "mobile";
-    }
-    return "desktop";
-};
