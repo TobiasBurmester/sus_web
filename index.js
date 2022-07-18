893c2175390cfd4a689f68a268f710db2a18fad2
Tobias Burmester <tobias.burmester@outlook.de>
2022-07-18 11:30:07 +0200

    Test DB Link

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-if ("serviceWorker" in navigator){
-    navigator.serviceWorker.register("sw.js").then(registration =>{
-        console.log("SW Registered!");
-        console.log(registration);
-    }).catch (error => {
-        console.log("SW Registration Failed");
-        console.log(error);
-    });
-}
-
-
-
-
 const { Client } = require('pg');
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
@@ -44,3 +31,15 @@ async function listUsers(req, res) {
         res.send("Error " + err);
     }
 }
+
+if ("serviceWorker" in navigator){
+    navigator.serviceWorker.register("sw.js").then(registration =>{
+        console.log("SW Registered!");
+        console.log(registration);
+    }).catch (error => {
+        console.log("SW Registration Failed");
+        console.log(error);
+    });
+}
+
+
