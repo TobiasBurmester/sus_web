27a940b3b842d0abcb3cca623964d08a6688ced2
Tobias Burmester <tobias.burmester@outlook.de>
2022-07-20 08:28:25 +0200

    Test DB Link

--- src/db_con.php
+++ src/db_con.php
@@ -1,16 +1,28 @@
 <?php
+        $db_connection = pg_connect(getenv(DATABASE_URL);
+        $result = pg_query($db_connection, "SELECT name FROM users");
 
-    $dbopts = parse_url(getenv('DATABASE_URL'));
-    $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-                   array(
-                    'pdo.server' => array(
-                       'driver'   => 'pgsql',
-                       'user' => $dbopts["user"],
-                       'password' => $dbopts["pass"],
-                       'host' => $dbopts["host"],
-                       'port' => $dbopts["port"],
-                       'dbname' => ltrim($dbopts["path"],'/')
-                       )
-                   )
-    );
+
+
+
+
+
+
+
+
+
+
+//   $dbopts = parse_url(getenv('DATABASE_URL'));
+ //     $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+ //                  array(
+  //                  'pdo.server' => array(
+   //                    'driver'   => 'pgsql',
+    //                   'user' => $dbopts["user"],
+     //                  'password' => $dbopts["pass"],
+//                       'host' => $dbopts["host"],
+//                       'port' => $dbopts["port"],
+//                      'dbname' => ltrim($dbopts["path"],'/')
+//                       )
+//                   )
+//    );
 ?>
